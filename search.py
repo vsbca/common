692418b9bf8a9999438b68dd7e5dcca68a9e4f65
vsbca <virendra.bisht@outlook.com>
2017-01-10 13:32:26 -0500

    Add files via upload

---
+++ search.py
@@ -0,0 +1,17 @@
+import os
+
+file1 = os.getcwd() + "\\" + "googleext.txt";
+print file1;
+datafile = os.getcwd() + "\\" + "rule_ext.txt";
+
+with open(datafile, "r") as f:
+    names = set(i.strip() for i in f)
+output = [];
+with open(file1, "r") as f:
+    for name in f:
+        if name.strip() not in names:
+            #print name;
+			output.append(name);
+print output
+		
+			
